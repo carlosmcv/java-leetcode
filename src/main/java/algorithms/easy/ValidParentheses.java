e993ba1253573bc069ca1909bc83726d1a4f16e2
carlosmcv <carlosmcv@gmail.com>
2018-05-07 20:18:43 -0400

    Valid Parentheses, another approach

--- src/main/java/algorithms/easy/ValidParentheses.java
+++ src/main/java/algorithms/easy/ValidParentheses.java
@@ -35,4 +35,24 @@ public class ValidParentheses {
             default: return ' ';
         }
     }
+
+
+    public static boolean run2(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
 }
