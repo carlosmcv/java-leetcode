c36ab9bc22e09c7d57266e6f10ea8e83066badf0
carlosmcv <carlosmcv@gmail.com>
2018-05-08 19:57:28 -0400

    Next Greater Element I, good solution

--- src/main/java/algorithms/easy/NextGreaterElementI.java
+++ src/main/java/algorithms/easy/NextGreaterElementI.java
@@ -1,6 +1,8 @@
 //Next Greater Element I
 package algorithms.easy;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -11,47 +13,32 @@ public class NextGreaterElementI {
 
     public static int[] run(int[] nums1, int[] nums2) {
         
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        if (nums1.length == 0)
+            return nums1;
+        
         int[] result = new int[nums1.length];
-
-        int max = nums2[nums2.length - 1];
         
-        stack1.push(-1);
-        stack1.push(max);
-        
-        for (int i = nums2.length - 2; i >= 0; i--) {
+        for (int i = 0; i < nums2.length; i++) {
             
-            if (nums2[i] < max) {
-                stack1.push(max);
-            } else {
-                stack1.push(-1);
-                max = nums2[i];
+            int n = nums2[i];
+            
+            while (!stack.isEmpty() && stack.peek() < n) {
+                map.put(stack.pop(), n);
             }
             
-            stack1.push(nums2[i]);          
+            stack.push(n);
         }
         
         for (int i = 0; i < nums1.length; i++) {
             int n = nums1[i];
             
-            boolean found = false;
-            
-            while (!stack1.isEmpty() && !found) {
-
-                int top = stack1.pop();
-                
-                if (top == n) {
-                    result[i] = stack1.pop();
-                    found = true;
-                } else {
-                    stack2.push(top);
-                    stack2.push(stack1.pop());
-                }
-            }
-            
-            while (!stack2.isEmpty())
-                stack1.push(stack2.pop());
+            if (map.get(n) != null)
+                result[i] = map.get(n);
+            else
+                result[i] = -1;      
         }
         
         return result;
